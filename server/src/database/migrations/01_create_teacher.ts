0e6d69f6aaa3374d135f16edc5fdc1296e43cb17
erik-ferreira <erik.souzagf9@gmail.com>
2021-02-24 13:41:55 -0300

    teacher migration updated

--- server/src/database/migrations/01_create_teacher.ts
+++ server/src/database/migrations/01_create_teacher.ts
@@ -3,6 +3,8 @@ import Knex from 'knex'
 export async function up(knex: Knex) {
   return knex.schema.createTable('teachers', table => {
     table.increments('id').primary();
+    table.string('name').notNullable();
+    table.string('surname').notNullable();
     table.string('image').notNullable();
     table.string('whatsapp').notNullable();
     table.string('bio').notNullable();
