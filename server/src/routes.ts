6c8a50b2d3031358d68334a9cac4e0ddc592c90b
erik-ferreira <erik.souzagf9@gmail.com>
2021-02-25 17:03:35 -0300

    routes updated

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,11 +2,13 @@ import express from 'express'
 import ConnectionController from './controllers/ConnectionController'
 import UserController from './controllers/UserController'
 import TeacherController from './controllers/TeacherController'
+import ClassesController from './controllers/ClassesController'
 
 const routes = express.Router()
 const userController = new UserController()
 const connectionController = new ConnectionController()
 const teacherController = new TeacherController()
+const classesController = new ClassesController()
 
 routes.get('/users', userController.show)
 routes.post('/users', userController.create)
@@ -14,7 +16,8 @@ routes.post('/users', userController.create)
 routes.get('/connections', connectionController.index)
 routes.post('/connections', connectionController.create)
 
-routes.post('/teachers/classes', teacherController.create)
+routes.post('/teachers', teacherController.create)
 routes.put('/teachers', teacherController.update)
+routes.delete('/teachers/:id', classesController.delete)
 
 export default routes
