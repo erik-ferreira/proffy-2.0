8b8c4288dff50b8502cc4c49a548a855fff1cb9b
erik-ferreira <erik.souzagf9@gmail.com>
2021-02-25 15:18:10 -0300

    routes in development

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,20 @@
 import express from 'express'
 import ConnectionController from './controllers/ConnectionController'
 import UserController from './controllers/UserController'
+import TeacherController from './controllers/TeacherController'
 
 const routes = express.Router()
 const userController = new UserController()
 const connectionController = new ConnectionController()
+const teacherController = new TeacherController()
 
 routes.get('/users', userController.show)
 routes.post('/users', userController.create)
 
 routes.get('/connections', connectionController.index)
 routes.post('/connections', connectionController.create)
+
+routes.post('/teachers/classes', teacherController.create)
+routes.put('/teachers', teacherController.update)
 
 export default routes
