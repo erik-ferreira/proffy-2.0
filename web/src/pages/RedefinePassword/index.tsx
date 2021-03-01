2dc329d34967fc0dbd930a112e6c2f5881e7aa07
erik-ferreira <erik.souzagf9@gmail.com>
2021-03-01 18:20:25 -0300

    RedefinePassword updated

--- web/src/pages/RedefinePassword/index.tsx
+++ web/src/pages/RedefinePassword/index.tsx
@@ -1,7 +1,42 @@
 import React from 'react'
+import { HiOutlineArrowNarrowLeft } from 'react-icons/hi'
+import Input from '../../components/Input'
+import InputPassword from '../../components/InputPassword'
+import PageHeader from '../../components/PageHeader'
+
+import './style.css'
 
 export default function RedefinePassword() {
   return (
-    <h1>redefinir</h1>
+    <div id="page-redefine-password">
+      <PageHeader>
+        <button>
+          <HiOutlineArrowNarrowLeft size={35} color="#BDA5F6" />
+        </button>
+      </PageHeader>
+
+      <form
+        id="form-redefine-password"
+      >
+        <button id="back">
+          <HiOutlineArrowNarrowLeft size={35} color="#8257E5" />
+        </button>
+
+        <fieldset>
+          <legend>Vamos redefinir sua senha!</legend>
+
+          <div>
+            <Input label="Código enviado para seu e-mail" type="text" />
+
+            <InputPassword />
+
+            <InputPassword labelInput="Repetir senha" />
+          </div>
+
+          <button type="submit">Redefinir</button>
+          
+        </fieldset>
+      </form>
+    </div>
   )
 }
