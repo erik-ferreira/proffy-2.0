4761280394970199016c9c736649ef2bc088f6c4
erik-ferreira <erik.souzagf9@gmail.com>
2021-03-02 16:52:10 -0300

    migration create_user updated

--- server/src/database/migrations/00_create_user.ts
+++ server/src/database/migrations/00_create_user.ts
@@ -7,6 +7,7 @@ export async function up(knex: Knex) {
     table.string('surname').notNullable();
     table.string('email').notNullable();
     table.string('password').notNullable();
+    table.string('code').notNullable();
   })
 }
 
