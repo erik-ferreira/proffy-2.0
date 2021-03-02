92b88ae47ff402faf31f892667cf569576df00e8
erik-ferreira <erik.souzagf9@gmail.com>
2021-03-02 16:58:59 -0300

    upadted password in PasswordController.ts finished

--- server/src/controllers/PasswordController.ts
+++ server/src/controllers/PasswordController.ts
@@ -1,24 +1,35 @@
 import { Request, Response } from 'express'
 import crypto from 'crypto'
+import bycript from 'bcrypt'
 import db from '../database/connection'
 
 export default class PasswordController {
   async update(request: Request, response: Response) {
-    const { hashEmail, email, password } = request.body
+    const { hashSentEmail, email, password } = request.body
 
-    const users = await db('users').select('*')
-     
-    for(let user of users) {
-      if(user.email !== email) {
-        return response.status(400).send('E-mail não está cadastrado na nossa plataforma!')
+    const user = await db('users').select('email', 'code').where('email', email)
+
+    if(user.length !== 0) {
+      if(user[0].code === hashSentEmail) {
+
+        const saltRounds = 10;
+
+        bycript.hash(password, saltRounds, async (err, hash) => {
+          const code = crypto.randomBytes(6).toString('hex')
+        
+          const user = await db('users').update({
+            password: hash,
+            code
+          }).where('email', email)
+        
+          return response.json(user)
+        })
+
+      } else {
+        return response.status(400).send('Código incompatível!')
       }
-      break
+    } else {
+      return response.status(400).send('E-mail não está cadastrado na nossa plataforma!')
     }
-
-    const code = crypto.randomBytes(6).toString('hex')
-
-    const user = await db('users').where('email', email)
-
-    return response.json()
   }
 }
