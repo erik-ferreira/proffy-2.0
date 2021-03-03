b37667430142c5ab4aa0f8c850fe86926168a815
erik-ferreira <erik.souzagf9@gmail.com>
2021-03-02 23:19:17 -0300

    unnecessary columns removed

--- server/src/database/migrations/01_create_teacher.ts
+++ server/src/database/migrations/01_create_teacher.ts
@@ -3,9 +3,6 @@ import Knex from 'knex'
 export async function up(knex: Knex) {
   return knex.schema.createTable('teachers', table => {
     table.increments('id').primary();
-    table.string('name').notNullable();
-    table.string('surname').notNullable();
-    table.string('email').notNullable();
     table.string('whatsapp').notNullable();
     table.string('bio').notNullable();
     table.string('image').notNullable();
