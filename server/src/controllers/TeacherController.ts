6efb8efd50a8bbde432ff50b2ee194bcce3fde55
erik-ferreira <erik.souzagf9@gmail.com>
2021-03-02 23:20:16 -0300

    creation modifications changed

--- server/src/controllers/TeacherController.ts
+++ server/src/controllers/TeacherController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import bcrypt from 'bcrypt'
 import db from '../database/connection'
 
 import convertHoursToMinutes from '../utils/convertHoursToMinutes'
@@ -14,18 +15,12 @@ export default class TeacherController {
   async create(request: Request, response: Response) {
     const { whatsapp, bio, subject, cost, schedule } = request.body
     const { user_id } = request.headers
-    
-    const user = await db('users').select('*').where('id', user_id)
-    const { name, surname, email } = user[0]
 
     const trx = await db.transaction()
     
     try {
       const insertedTeachersIds = await trx('teachers').insert({
         user_id,
-        name, 
-        surname,
-        email,
         whatsapp,
         bio,
         image: 'image.jpg',
@@ -66,7 +61,7 @@ export default class TeacherController {
 
   async update(request: Request, response: Response) {
     const { teacher_id, class_id, schedule_id } = request.headers
-    const { name, surname, email, whatsapp, bio, cost, schedule } = request.body
+    const { name, surname, currentPassword, newPassword, email, whatsapp, bio, cost, schedule } = request.body
 
     const arrayScheduleIds = String(schedule_id).split(',').map(id => id.trim())
 
@@ -80,10 +75,29 @@ export default class TeacherController {
     })
 
     try {
+      const user = await db('teachers')
+      .join('users', 'teachers.user_id', '=', 'users.id')
+      .select('users.password', 'users.id')
+      .where('teachers.user_id', '=', teacher_id as string)
+
+      const password = user[0].password
+      bcrypt.compare(currentPassword, password, (err, result) => {
+        if(result) {
+          const saltRounds = 10
+          bcrypt.hash(newPassword, saltRounds, async (err, hash) => {
+            await db('users')
+              .update({ 
+                password: hash,
+                name,
+                surname,
+              })
+              .where('id', user[0].id)
+          })
+        }
+      })
+
       await db('teachers')
         .update({
-          name,
-          surname,
           email, 
           whatsapp,
           bio
